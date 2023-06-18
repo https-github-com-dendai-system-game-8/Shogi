50abe2d83ca4618be55598a4700868137efb4512
KonishiMotoharu <supermanrg2@gmail.com>
2023-06-18 23:31:22 +0900

    camera_update
    カメラの回転の向きを手動で変更できるようにしました

--- Project/Assets/Script/SoloMode/CameraManager.cs
+++ Project/Assets/Script/SoloMode/CameraManager.cs
@@ -5,36 +5,45 @@ using UnityEngine.UIElements;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform m_target;
-    [SerializeField] private int speed;
+    public Transform m_target;//âÒì]ÇÃíÜêSé≤
+    [SerializeField] private int speed;//ëOå„Ç∑ÇÈë¨Ç≥
     public float m_rotateSpeed = 10;
     private GameObject otherCamera;
+    [SerializeField] private Vector3 axis = new Vector3(0,0,1);
+
     private void Start()
     {
         if (tag == "MainCamera")
             otherCamera = GameObject.FindGameObjectWithTag("SubCamera");
         else
             otherCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if (axis.x != 0)
+            axis = new Vector3(axis.x / axis.x, axis.y, axis.z);
+        if (axis.y != 0)
+            axis = new Vector3(axis.x, axis.y / axis.y, axis.z);
+        if (axis.z != 0)
+            axis = new Vector3(axis.x, axis.y, axis.z / axis.z);
     }
     private void Update()
     {
         if(tag == "MainCamera" && otherCamera.activeInHierarchy)
         {
-            transform.RotateAround(m_target.position, Vector3.forward, m_rotateSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime);
-            transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * speed * Time.deltaTime);
+            transform.RotateAround(m_target.position, axis, m_rotateSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime);
+            transform.Translate(Input.GetAxis("Vertical") * m_target.position * speed * Time.deltaTime);
             //transform.LookAt(m_target);
         }
         else if(tag == "SubCamera" && otherCamera.activeInHierarchy)
         {
-            transform.RotateAround(m_target.position, Vector3.forward, m_rotateSpeed * -Input.GetAxis("Horizontal2") * Time.deltaTime);
-            transform.Translate(Input.GetAxis("Vertical2") * Vector3.forward * speed * Time.deltaTime);
+            transform.RotateAround(m_target.position, axis, m_rotateSpeed * -Input.GetAxis("Horizontal2") * Time.deltaTime);
+            transform.Translate(Input.GetAxis("Vertical2") * m_target.position * speed * Time.deltaTime);
             //transform.LookAt(m_target);
         }
         else
         {
-            transform.RotateAround(m_target.position, Vector3.forward, m_rotateSpeed * -Input.GetAxis("Horizontal2") * Time.deltaTime);
-            transform.Translate(Input.GetAxis("Vertical2") * Vector3.forward * speed * Time.deltaTime);
-            transform.RotateAround(m_target.position, Vector3.forward, m_rotateSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime);
+            transform.RotateAround(m_target.position, axis, m_rotateSpeed * -Input.GetAxis("Horizontal2") * Time.deltaTime);
+            transform.Translate(Input.GetAxis("Vertical2") * m_target.position * speed * Time.deltaTime);
+            transform.RotateAround(m_target.position, axis, m_rotateSpeed * -Input.GetAxis("Horizontal") * Time.deltaTime);
             transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * speed * Time.deltaTime);
         }
     }
