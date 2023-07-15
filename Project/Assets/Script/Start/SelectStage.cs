847fccf3250d570f97c6b3c79f3cc45d26318685
KonishiMotoharu <supermanrg2@gmail.com>
2023-07-15 21:15:12 +0900

    2PGazoUpdate
    2P画像を駒に割り当てました

--- Project/Assets/Script/Start/SelectStage.cs
+++ Project/Assets/Script/Start/SelectStage.cs
@@ -8,16 +8,19 @@ public class SelectStage : MonoBehaviour
     public int stageNum;
     private Image stageImage;
     [SerializeField]private Sprite[] stageSprite;
+    private RectTransform re;
     // Start is called before the first frame update
     void Start()
     {
         stageImage = GetComponent<Image>();
+        re = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         stageImage.sprite = stageSprite[stageNum - 1];
+        re.sizeDelta = stageImage.sprite.textureRect.size;
     }
 
     public void OnClick(int num)
